0090fba33b524d150641f88a2e73446404997b2f
Alisher <alisheryondoshaliyev77@gmail.com>
2025-11-08 23:12:13 +0500

    feat: add initial project skeletons for all apps

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,22 +1,19 @@
 import js from '@eslint/js'
 import globals from 'globals'
 import { defineConfig } from 'eslint/config'
-
-// Prettier'ni import qilamiz
 import prettierConfig from 'eslint-config-prettier'
 
 export default defineConfig([
   {
     files: ['**/*.{js,mjs,cjs}'],
     plugins: { js },
-    extends: ['js/recommended'],
+
+    // MANA SHU YERDA XATO BOR EDI (QO'SHTIRNOQLAR OLIB TASHLANDI)
+    extends: [js.configs.recommended],
+
     languageOptions: {
-      // Buni "node" ga o'zgartiramiz, chunki bu server loyihasi
       globals: globals.node,
     },
   },
-
-  // Prettier'ni eng oxiriga qo'shamiz. Bu ESLint'ga
-  // formatlashni Prettier'ga topshirishni aytadi.
   prettierConfig,
 ])
